890c85b4968a0ac931756e78acf9da5531d6c710
Harshit Pandey <harshitpandey678@gmail.com>
2017-12-09 17:17:34 +0530

    clean file

--- print.js
+++
@@ -1,5 +0,0 @@
-function printDoc()
-  {
-    window.frames["textfield"].focus(); 
-    window.frames["textfield"].print();
-  }
--- zoom.js
+++
@@ -1,6 +0,0 @@
- function zooM(){
-        var slidevalue=document.getElementById('slidebar').value;
-        var currentzoom=1.1+(slidevalue/100.0);
-        $('#editor2').css("zoom",currentzoom);
-        $("#editor2").css("-moz-transform-origin", "0 0");
-     }
